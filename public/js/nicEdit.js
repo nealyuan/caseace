370f974c616147c1e2d05b6fad8c7b4bd659fe0f
Neal Yuan <yuanneal@gmail.com>
2014-01-12 19:47:11 -0800

    changed order of displaying elements in the answer page. Also fixed so that icons display in textarea editing spaces by fixing the path for icons to point to root folder

--- public/js/nicEdit.js
+++ public/js/nicEdit.js
@@ -268,7 +268,7 @@ var nicEditorConfig = bkClass.extend({
 		'outdent' : {name : __('Remove Indent'), command : 'outdent', noActive : true},
 		'hr' : {name : __('Horizontal Rule'), command : 'insertHorizontalRule', noActive : true}
 	},
-	iconsPath : '../public/img/nicEditorIcons.gif',
+	iconsPath : '/public/img/nicEditorIcons.gif',
 	buttonList : ['save','bold','italic','underline','left','center','right','justify','ol','ul','fontSize','fontFamily','fontFormat','indent','outdent','upload','link','unlink','forecolor','xhtml'],
 	iconList : {"xhtml":1,"bgcolor":2,"forecolor":3,"bold":4,"center":5,"hr":6,"indent":7,"italic":8,"justify":9,"left":10,"ol":11,"outdent":12,"removeformat":13,"right":14,"save":25,"strikethrough":16,"subscript":17,"superscript":18,"ul":19,"underline":20,"image":21,"link":22,"unlink":23,"close":24,"arrow":26,"upload":27}
 	
